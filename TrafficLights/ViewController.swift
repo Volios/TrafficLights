82c83eeea928c5e6f2b74e0364f9d2ad114e5a31
Volios <gilbertbytes@gmail.com>
2023-06-09 23:19:57 +0300

    Final version, append switch statement

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+enum Light {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = Light.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = 60
+        yellowLightView.layer.cornerRadius = 60
+        greenLightView.layer.cornerRadius = 60
+        
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func changeLightsButton() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLightView.alpha = 1
+            currentLight = .yellow
+            greenLightView.alpha = 0.3
+        case .yellow:
+            yellowLightView.alpha = 1
+            currentLight = .green
+            redLightView.alpha = 0.3
+        case .green:
+            greenLightView.alpha = 1
+            currentLight = .red
+            yellowLightView.alpha = 0.3
+        }
+        
+    }
+    
 }
 
